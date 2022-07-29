abecb279f99f5fcebfc4754857bdf4e2b8263500
kirbi <Biehan@mail.ru>
2022-07-29 09:48:09 +0300

    start work with db (again)

--- app.py
+++ app.py
@@ -8,8 +8,19 @@ from flask_sqlalchemy import SQLAlchemy
 def load_key():
 	return open('setup/secret_key.txt', 'r').read()
 
+def connect_to_db():
+    args = []
+    setup_f = open('setup/server_setup.txt', 'r')
+    for el in setup_f:
+        args.append(el.replace('\n',''))
+    return 'postgresql://{}:{}@{}:{}/{}'.format(args[0], args[1], args[2], args[3], args[4])
+    
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = load_key()
+app.config['SQLALCHEMY_DATABASE_URI'] = connect_to_db()
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 @app.route('/')
 def main():
