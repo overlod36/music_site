93fcc5ab13bcaf1d8d9fc577a0879028e28519dc
kirbi <Biehan@mail.ru>
2022-08-05 22:41:33 +0300

    filling script is almost done

--- filling_script.py
+++ filling_script.py
@@ -1,5 +1,6 @@
 from pack import db, models
 import os, sys, shutil
+import json
 
 def menu():
 	print('[МЕНЮ]\n1]Сброс базы данных\n2]Заполнение базы данных\n3]Прекращение работы')
@@ -12,11 +13,23 @@ while True:
 			db.drop_all()
 			db.create_all()
 			if os.path.exists('pack/static/models_images'):
-                                shutil.rmtree('pack/static/models_images')
+				shutil.rmtree('pack/static/models_images')
 			if not os.path.exists('pack/static/models_images'):
 				os.mkdir('pack/static/models_images')
+			print('Сброс базы данных!')
 			continue
 		case '2':
+			with open('pack/setup/fill_db/groups.json', encoding="utf-8") as file:
+				groups = json.load(file)
+				for el in groups:
+					if el.has_key('past_members'):
+						db.session.add(models.Group(name=el['name'], main_cover_path=el['main_cover_path'], addin_cover_path=el['addin_cover_path'], logo_path=el['logo_path'], current_members=el['current_members'], past_members=el['past_members'], active_years=el['active_years']))
+					else:
+						db.session.add(models.Group(name=el['name'], main_cover_path=el['main_cover_path'], addin_cover_path=el['addin_cover_path'], logo_path=el['logo_path'], current_members=el['current_members'], active_years=el['active_years']))			
+			with open('pack/setup/fill_db/albums.json', encoding="utf-8") as file:
+				albums = json.load(file)
+				for el in albums:
+					db.session.add(models.Album(title=el['title'], release_date))
 			print('Заполнение базы данных!')
 			continue
 		case '3':
--- pack/models.py
+++ pack/models.py
@@ -6,7 +6,12 @@ class Group(db.Model):
 	__tablename__ = "group"
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(100), unique=True, nullable=False)
-	cover_path = db.Column(db.String(100), nullable=False)
+	main_cover_path = db.Column(db.String(100), nullable=False)
+	addin_cover_path = db.Column(db.String(100))
+	logo_path = db.Column(db.String(100))
+	current_members = db.Column(ARRAY(db.String(100)), nullable=False)
+	past_members = db.Column(ARRAY(db.String(100)))
+	active_years = db.Column(db.String(100), nullable=False)
 	al_link = db.relationship('Album', backref='group')
 
 class Album(db.Model):
@@ -19,7 +24,7 @@ class Album(db.Model):
 	group_name = db.Column(db.String(100), db.ForeignKey('group.name'), unique=False, nullable=False)
 	songs = db.Column(ARRAY(db.String(100)), nullable=False)
 	cover_path = db.Column(db.String(100), nullable=False)
-
+	genres = db.Column(ARRAY(db.String(100)), nullable=False)
 	def __repr__(self):
 		return f'Album |{self.title}|\n release date >> {self.release_date}\n duration >> {self.duration}'
 
