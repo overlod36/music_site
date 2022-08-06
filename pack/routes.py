40b397ba3ebb269f55f497cadebfef6098bf38d6
kirbi <Biehan@mail.ru>
2022-08-06 17:55:53 +0300

    now addin works normally

--- pack/routes.py
+++ pack/routes.py
@@ -15,7 +15,7 @@ def get_time(st):
 def get_list(st):
 	return(list(st.replace(', ', '|').split('|')))
 
-def group_file_saver(field, group_name):
+def file_saver(field, group_name):
 	file = request.files[field]
 	filename = secure_filename(file.filename)
 	file.save(os.path.join(f'pack/static/models_images/{group_name}' , filename))
@@ -31,10 +31,7 @@ def main():
 def add_alb():
 	form = forms.Album_Form()
 	if request.method == 'POST':
-		file = request.files['cover_field']
-		filename = secure_filename(file.filename)
-		file.save(os.path.join(f'pack/models_images/{form.group_field.data}', filename))
-		new_album = models.Album(title=form.title_field.data, group_name=form.group_field.data, release_date=form.date_field.data, producers_name=form.producers_field.data, duration=get_time(str(form.duration_field.data)[11:]), songs=get_list(form.songs_field.data), cover_path=filename, active_years=get_list(form.genres_field.data))
+		new_album = models.Album(title=form.title_field.data, group_name=form.group_field.data, release_date=form.date_field.data, producers_name=form.producers_field.data, duration=get_time(str(form.duration_field.data)[11:]), songs=get_list(form.songs_field.data), cover_path=file_saver('cover_field', form.group_field.data), genres=get_list(form.genres_field.data))
 		db.session.add(new_album)
 		db.session.commit()
 		return redirect(url_for('main'))
@@ -46,9 +43,7 @@ def add_group():
 	if request.method == 'POST':
 		if not os.path.exists(f'pack/static/models_images/{form.name_field.data}'):
 			os.mkdir(f'pack/static/models_images/{form.name_field.data}')
-
-		st = [group_file_saver('cover_field', form.name_field.data), group_file_saver('add_cover_field', form.name_field.data), group_file_saver('logo_field', form.name_field.data)]
-		new_group = models.Group(name=form.name_field.data, main_cover_path=st[0], addin_cover_path=st[1], logo_path=st[2], current_members=get_list(form.current_members_field.data), past_members=get_list(form.past_members_field.data), active_years=form.active_years.data)
+		new_group = models.Group(name=form.name_field.data, main_cover_path=file_saver('cover_field', form.name_field.data), addin_cover_path=file_saver('add_cover_field', form.name_field.data), logo_path=file_saver('logo_field', form.name_field.data), current_members=get_list(form.current_members_field.data), past_members=get_list(form.past_members_field.data), active_years=form.active_years.data)
 		db.session.add(new_group)
 		db.session.commit()
 		return redirect(url_for('main'))
