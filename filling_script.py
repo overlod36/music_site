bdf4eeb623fe3e78a5faa1b00bbe90be1d14ce6d
kirbi <Biehan@mail.ru>
2022-08-06 09:57:32 +0300

    now we can fill db by filling script from json files

--- filling_script.py
+++ filling_script.py
@@ -1,6 +1,7 @@
 from pack import db, models
 import os, sys, shutil
 import json
+import datetime as dt
 
 def menu():
 	print('[МЕНЮ]\n1]Сброс базы данных\n2]Заполнение базы данных\n3]Прекращение работы')
@@ -22,14 +23,15 @@ while True:
 			with open('pack/setup/fill_db/groups.json', encoding="utf-8") as file:
 				groups = json.load(file)
 				for el in groups:
-					if el.has_key('past_members'):
+					if 'past_members' in el.keys():
 						db.session.add(models.Group(name=el['name'], main_cover_path=el['main_cover_path'], addin_cover_path=el['addin_cover_path'], logo_path=el['logo_path'], current_members=el['current_members'], past_members=el['past_members'], active_years=el['active_years']))
 					else:
 						db.session.add(models.Group(name=el['name'], main_cover_path=el['main_cover_path'], addin_cover_path=el['addin_cover_path'], logo_path=el['logo_path'], current_members=el['current_members'], active_years=el['active_years']))			
 			with open('pack/setup/fill_db/albums.json', encoding="utf-8") as file:
 				albums = json.load(file)
 				for el in albums:
-					db.session.add(models.Album(title=el['title'], release_date))
+					db.session.add(models.Album(title=el['title'], release_date=dt.datetime.strptime(el['release_date'], "%d.%m.%Y").date(), producers_name=el['producers_name'], duration=dt.datetime.strptime(el['duration'], "%H:%M:%S").time(), songs=el['songs'], group_name=el['group_name'], cover_path=el['cover_path'], genres=el['genres']))
+			db.session.commit()
 			print('Заполнение базы данных!')
 			continue
 		case '3':
